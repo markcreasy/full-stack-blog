f9d7d8d5b0d989253b7b3314c03c842019916e79
Mark Creasy <mcreasy50@gmail.com>
2022-06-06 14:27:09 -0400

    post comment order asc

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,8 +41,9 @@ router.get('/post/:id', (req,res) => {
       where:{id:req.params.id},
       include:{
         model: Comment,
-        include: User
-      }
+        include: User,
+      },
+      order:[[Comment,'createdAt','DESC']]
     }
   ).then(data => {
       // convert data to array
